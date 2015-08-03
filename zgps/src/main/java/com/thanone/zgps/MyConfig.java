e270f9bf2d44b24b83d9d3e0064e956cab10c93e
zouchongjin@sina.com <zouchongjin@sina.com>
2015-08-03 21:31:17 +0800

    ...

--- zgps/src/main/java/com/thanone/zgps/MyConfig.java
+++ zgps/src/main/java/com/thanone/zgps/MyConfig.java
@@ -4,9 +4,11 @@ import android.util.Log;
 
 public class MyConfig {
 
-	public static String URLBASE = "http://139.196.12.108:8080/zgpscms/app/";
+	public static String URLBASE = BuildConfig.URLBASE;
 
 	public static void log(String string) {
-		Log.v("zgps", string);
+		if(BuildConfig.LOG_DEBUG) {
+			Log.v("zgps", string);
+		}
 	}
 }

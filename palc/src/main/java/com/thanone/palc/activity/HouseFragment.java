65579864a9d2725000adea966fa0454dfa10a5d4
zouchongjin@sina.com <zouchongjin@sina.com>
2015-09-14 18:10:47 +0800

    ...

--- palc/src/main/java/com/thanone/palc/activity/HouseFragment.java
+++ palc/src/main/java/com/thanone/palc/activity/HouseFragment.java
@@ -1,6 +1,7 @@
 package com.thanone.palc.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,19 +12,43 @@ import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.thanone.palc.MyApplication;
 import com.thanone.palc.MyConfig;
 import com.thanone.palc.R;
+import com.zcj.android.util.PhotoChooseUtils;
+import com.zcj.android.util.UtilImage;
+import com.zcj.util.UtilString;
 
 public class HouseFragment extends Fragment {
 
     private MyApplication application;
     private MainsActivity activity;
 
+    private PhotoChooseUtils photoChooseUtils;
+
     @ViewInject(R.id.header_title)
     private TextView header_title;
     @ViewInject(R.id.header_back)
     private ImageView header_back;
+
+    @ViewInject(R.id.house_idcardimg)
+    private ImageView house_idcardimg;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String imgPath = photoChooseUtils.onActivityResult(requestCode, data);
+        if (UtilString.isNotBlank(imgPath)) {
+            house_idcardimg.setVisibility(View.VISIBLE);
+            house_idcardimg.setImageDrawable(UtilImage.getDrawableByFilePath(imgPath));
+        }
+    }
+
+    @OnClick(R.id.house_idcard)
+    private void house_idcard(View v) {
+        photoChooseUtils.show();
+    }
 
     @Override
     public void onAttach(Activity activity) {
@@ -54,6 +79,8 @@ public class HouseFragment extends Fragment {
 
         header_title.setText("出租房登记");
         header_back.setVisibility(View.GONE);
+
+        photoChooseUtils = new PhotoChooseUtils(getActivity(), this, false);
 
         MyConfig.log("HouseFragment onActivityCreated");
     }
@@ -103,4 +130,5 @@ public class HouseFragment extends Fragment {
         super.onDestroy();
         MyConfig.log("HouseFragment onDestroy");
     }
+
 }
